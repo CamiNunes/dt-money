e39997dc0bcf50b8324d1f71226c6b3d30c68bff
CamiNunes <cvn.camila@gmail.com>
2023-03-19 16:12:49 -0300

    Carregando dados do backend json-server;

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,58 @@
+import { useContext, useEffect, useState } from "react";
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
+
+interface Transaction{
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
+export function Transactions(){
+  
+  const { transactions } = useContext(TransactionsContext);
+
+  return(
+    <div>
+      <Header/>  
+      <Summary/>
+      <TransactionsContainer>
+        <SearchForm/>
+        <TransactionsTable>
+          <tbody>
+            {transactions.map(transaction => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighLight variant={transaction.type}>
+                      {transaction.price}
+                    </PriceHighLight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                  {/* <td>
+                    <ButtonSave>Editar</ButtonSave>
+                  </td>
+                  <td>
+                    <ButtonSave>Editar</ButtonSave>
+                  </td> */}
+                </tr>
+              )
+            })}  
+
+            
+            
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+      
+    </div>
+  )
+}
